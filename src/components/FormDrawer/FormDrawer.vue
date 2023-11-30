d10e0c68bdac2e67ec29d88972f39bf6026ec592
hanyucd <1807003141@qq.com>
2023-11-30 22:32:17 +0800

    feat: 封装 drawer 弹窗组件

---
+++ src/components/FormDrawer/FormDrawer.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-drawer v-model="isShowDrawer" :title="title" :destroy-on-close="destroyOnClose">
+    <div class="form-drawer-body">
+      <div class="form-drawer-main">
+        <slot></slot>
+      </div>
+
+      <div class="form-drawer-footer">
+        <el-button type="primary" size="large" class="w-[120px]" :loading="submitBtnLoading" @click="submitEvt">{{ confirmText }}</el-button>
+        <el-button type="warning" size="large" class="w-[120px]">取 消</el-button>
+      </div>
+    </div>
+  </el-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  title: String,
+  size: { type: String, default: '40%' },
+  confirmText: { type: String, default: '提交' },
+  destroyOnClose: { type: Boolean, default: false }
+});
+
+const emit = defineEmits(['formDrawerSubmitEvt']);
+
+const isShowDrawer = ref(true); // 是否显示 drawer
+const submitBtnLoading = ref(false); // 提交按钮 loading
+
+/**
+ * 打开提交按钮状态
+ */
+const showSubmitBtnLoading = () => submitBtnLoading.value = true;
+/**
+ * 关闭提交按钮状态
+ */
+const hideSubmitBtnLoading = () => submitBtnLoading.value = false;
+
+/**
+ * 确定提交
+ */
+const submitEvt = () => emit('formDrawerSubmitEvt');
+
+/**
+ * 打开 drawer
+ */
+const openFormDrawer = () => isShowDrawer.value = true;
+
+/**
+ * 关闭 drawer
+ */
+ const closeFormDrawer = () => {
+   isShowDrawer.value = false;
+ };
+
+//  向父级暴露一下方法
+ defineExpose({
+  openFormDrawer,
+  closeFormDrawer,
+  showSubmitBtnLoading,
+  hideSubmitBtnLoading
+});
+</script>
+
+<style lang="scss" scoped>
+  @import './style.scss';
+</style>
