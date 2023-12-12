017424f9fa2985f1f437e70141b5fb69faa83d8b
hanyucd <1807003141@qq.com>
2023-12-12 16:24:45 +0800

    feat: 新增商品列表 tab

--- src/views/module-goods/goods-manage/goods-manage.vue
+++ src/views/module-goods/goods-manage/goods-manage.vue
@@ -1,98 +1,104 @@
 <template>
-  <el-card shadow="never" class="border-0">
-    <!-- 搜索 -->
-    <SearchWrap :form-model="searchForm" @searchEvt="getTableData" @resetEvt="resetSearchForm">
-      <SearchItem label="关键词">
-        <el-input v-model="searchForm.keyword" clearable placeholder="请输入管理员昵称搜索" />
-      </SearchItem>
-    </SearchWrap>
+  <div>
+    <el-tabs v-model="searchForm.tab" class="demo-tabs" @tab-change="onTabChangeEvt">
+      <el-tab-pane v-for="(item, index) in tabbars" :key="index" :label="item.name" :name="item.key" />
+    </el-tabs>
     
-    <TableHeader @createEvt="openFormDrawer" @refreshEvt="getTableData(tablePage)" />
-    
-    <!-- 表格数据 -->
-    <el-table v-loading="tableIsLoading" :data="tableDataList" border stripe>
-      <el-table-column type="selection" width="55" />
-
-      <!-- 管理员 -->
-      <el-table-column label="管理员" width="300">
-        <template #default="{ row }">
-          <div class="flex items-center">
-            <el-avatar :size="40" :src="row.avatar">
-              <img src="https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png" />
-            </el-avatar>
-            <div class="ml-2 leading-normal">
-              <h3>{{ row.username }}</h3>
-              <small class="mt-1">ID: {{ row.id }}</small>
-            </div>
-          </div>
-        </template>
-      </el-table-column>
+    <el-card shadow="never" class="border-0">
+      <!-- 搜索 -->
+      <SearchWrap :form-model="searchForm" @searchEvt="getTableData" @resetEvt="resetSearchForm">
+        <SearchItem label="关键词">
+          <el-input v-model="searchForm.title" clearable placeholder="请输入商品名称" />
+        </SearchItem>
+      </SearchWrap>
       
-      <el-table-column label="实际销量" width="100" prop="sale_count" align="center" />
-
-      <el-table-column prop="stock" label="总库存" width="110" align="center" />
-
-      <el-table-column label="状态" align="center">
-        <template #default="{ row }">
-          <el-switch :disabled="row.super == 1" :model-value="row.status" :active-value="1" :inactive-value="0" :loading="row.isLoading" @change="switchChange($event, row)" />
-        </template>
-      </el-table-column>
-
-      <el-table-column label="操作" width="260" align="center">
-        <template #default="scope">
-          <small v-if="scope.row.super == 1" class="text-gray-500">暂无操作</small>
-          <div v-else>
-            <el-button size="small" type="primary" @click="handleEditTableItem(scope.row)">修改</el-button>
-            <el-popconfirm title="是否删除该角色?" width="160" confirm-button-text="删除" cancel-button-text="取消" @confirm="handleTableItemDelete(scope.row)">
-              <template #reference>
-                <el-button size="small" type="danger">删除</el-button>
-              </template>
-            </el-popconfirm>
-          </div>
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <!-- 分页器 -->
-    <el-pagination
-      v-model:current-page="tablePage"
-      v-model:page-size="tableLimit"
-      class="mt-6 flex items-center justify-center"
-      background
-      layout="prev, pager, next" 
-      :total="tableTotal"
-      @current-change="onTableCurPaginationChangeEvt"
-    />
-
-    <!-- 表单Drawer -->
-    <FormDrawer ref="formDrawerRef" :title="formDrawerTitle" destroy-on-close @formDrawerSubmitEvt="onFormDrawerSubmitEvt" @formDrawerCloseEvt="onFormDrawerCloseEvt">
-      <el-form ref="formRef" :model="tableFormData" :rules="tableFormRules" label-width="80px" :inline="false" size="default">
-        <el-form-item label="头像" prop="avatar">
-          <!-- 组件 v-model 实现双向绑定 -->
-          <ChooseImage v-model="tableFormData.avatar" />
-        </el-form-item>
+      <TableHeader @createEvt="openFormDrawer" @refreshEvt="getTableData(tablePage)" />
+      
+      <!-- 表格数据 -->
+      <el-table v-loading="tableIsLoading" :data="tableDataList" border stripe>
+        <el-table-column type="selection" width="55" />
+  
+        <!-- 管理员 -->
+        <el-table-column label="管理员" width="300">
+          <template #default="{ row }">
+            <div class="flex items-center">
+              <el-avatar :size="40" :src="row.avatar">
+                <img src="https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png" />
+              </el-avatar>
+              <div class="ml-2 leading-normal">
+                <h3>{{ row.username }}</h3>
+                <small class="mt-1">ID: {{ row.id }}</small>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
         
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="tableFormData.username" placeholder="请输入用户名" />
-        </el-form-item>
-
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="tableFormData.password" placeholder="请输入密码" show-password />
-        </el-form-item>
-
-        <el-form-item label="角色" prop="role_id">
-          <el-select v-model="tableFormData.role_id" placeholder="请选择角色" clearable filterable>
-            <el-option label="--请选择角色--" value="--" disabled />
-            <el-option v-for="item in managerRoles" :key="item.id" :label="item.name" :value="item.id" />
-          </el-select>
-        </el-form-item>
-        
-        <el-form-item label="是否启用" prop="status">
-          <el-switch v-model="tableFormData.status" :active-value="1" :inactive-value="0" />
-        </el-form-item>
-      </el-form>
-    </FormDrawer>
-  </el-card>
+        <el-table-column label="实际销量" width="100" prop="sale_count" align="center" />
+  
+        <el-table-column prop="stock" label="总库存" width="110" align="center" />
+  
+        <el-table-column label="状态" align="center">
+          <template #default="{ row }">
+            <el-switch :disabled="row.super == 1" :model-value="row.status" :active-value="1" :inactive-value="0" :loading="row.isLoading" @change="switchChange($event, row)" />
+          </template>
+        </el-table-column>
+  
+        <el-table-column label="操作" width="260" align="center">
+          <template #default="scope">
+            <small v-if="scope.row.super == 1" class="text-gray-500">暂无操作</small>
+            <div v-else>
+              <el-button size="small" type="primary" @click="handleEditTableItem(scope.row)">修改</el-button>
+              <el-popconfirm title="是否删除该角色?" width="160" confirm-button-text="删除" cancel-button-text="取消" @confirm="handleTableItemDelete(scope.row)">
+                <template #reference>
+                  <el-button size="small" type="danger">删除</el-button>
+                </template>
+              </el-popconfirm>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+  
+      <!-- 分页器 -->
+      <el-pagination
+        v-model:current-page="tablePage"
+        v-model:page-size="tableLimit"
+        class="mt-6 flex items-center justify-center"
+        background
+        layout="prev, pager, next" 
+        :total="tableTotal"
+        @current-change="onTableCurPaginationChangeEvt"
+      />
+  
+      <!-- 表单Drawer -->
+      <FormDrawer ref="formDrawerRef" :title="formDrawerTitle" destroy-on-close @formDrawerSubmitEvt="onFormDrawerSubmitEvt" @formDrawerCloseEvt="onFormDrawerCloseEvt">
+        <el-form ref="formRef" :model="tableFormData" :rules="tableFormRules" label-width="80px" :inline="false" size="default">
+          <el-form-item label="头像" prop="avatar">
+            <!-- 组件 v-model 实现双向绑定 -->
+            <ChooseImage v-model="tableFormData.avatar" />
+          </el-form-item>
+          
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="tableFormData.username" placeholder="请输入用户名" />
+          </el-form-item>
+  
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="tableFormData.password" placeholder="请输入密码" show-password />
+          </el-form-item>
+  
+          <el-form-item label="角色" prop="role_id">
+            <el-select v-model="tableFormData.role_id" placeholder="请选择角色" clearable filterable>
+              <el-option label="--请选择角色--" value="--" disabled />
+              <el-option v-for="item in managerRoles" :key="item.id" :label="item.name" :value="item.id" />
+            </el-select>
+          </el-form-item>
+          
+          <el-form-item label="是否启用" prop="status">
+            <el-switch v-model="tableFormData.status" :active-value="1" :inactive-value="0" />
+          </el-form-item>
+        </el-form>
+      </FormDrawer>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
@@ -122,7 +128,11 @@ const {
   onTableCurPaginationChangeEvt,
   handleTableItemDelete
 } = useTableHook.useBaseTableHook({
-  searchForm: { keyword: '' },
+  searchForm: {
+    title: '',
+    tab: 'all',
+    category_id: null
+  },
   getTableDataApi: proxy.$api.getGoodsLisApi,
   updateStatusApi: proxy.$api.updateManagerStatusApi,
   deleteApi: proxy.$api.deleteManagerApi,
@@ -164,7 +174,25 @@ const {
   getTableData,
 });
 
+// tab 标签页
+const tabbars = [
+  { key: 'all', name: '全部' },
+  { key: 'checking', name: '审核中' },
+  { key: 'saling', name: '出售中' },
+  { key: 'off', name: '已下架' },
+  { key: 'min_stock', name: '库存预警' },
+  { key: 'delete', name: '回收站' }
+];
+
 getTableData();
+
+/**
+ * tab 改变时触发
+ */
+const onTabChangeEvt = activeTabName => {
+  console.log(activeTabName);
+  getTableData();
+};
 
 </script>
 
