8ef7437f497f8c4d0b9f2343f0ccf91610976ee1
hanyucd <1807003141@qq.com>
2023-12-15 17:41:43 +0800

    feat: 优化代码

--- src/hooks/useSkuHook.js
+++
@@ -1,7 +0,0 @@
-import { reactive, ref, computed } from 'vue';
-import * as commonUtil from '@/common/utils';
-
-/**
- * 表格查询、删除表格项、修改表格项状态
- */
-export const useBaseTableHook = (options = {}) => {};
